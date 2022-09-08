c5d19c9ff1f089b6692e2c4c00f8a8b0296c046a
hanmariyang <113072884+hanmariyang@users.noreply.github.com>
2022-09-08 15:39:07 +0900

    Update calculator.py ( 세부 주석 추가 )

--- calculator.py
+++ calculator.py
@@ -1,13 +1,13 @@
 
 # 함수 설정하기
 def plus(a, b):
-    return a + b
+    return a + b # 더하기
 
 def minus(a, b):
-    return a - b
+    return a - b # 뺴기
 
 def multiplied(a, b):
-    return a * b
+    return a * b # 곱하기
 
 def divided(a, b):
-    return a / b
+    return a / b # 나누기
